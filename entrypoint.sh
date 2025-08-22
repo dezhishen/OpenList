68b587a3a5eafecfcf4545ee75b5272ba17406a0
dezhishen <26274059+dezhishen@users.noreply.github.com>
2025-08-22 17:34:17 +0800

    fix(docker): add -x check

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,20 +5,20 @@ umask ${UMASK}
 if [ "$1" = "version" ]; then
   ./openlist version
 else
-  # Define the target directory path for aria2 service
   # Check file of /opt/openlist/data permissions for current user
-# 检查当前用户是否有当前目录的写权限
-  if ! [ -w ./data ]; then
+  # 检查当前用户是否有当前目录的写和执行权限
+  if ! [ -w ./data ] || ! [ -x ./data ]; then
     cat <<EOF
-Error: Current user does not have write permissions in the current directory: $(pwd)
+Error: Current user does not have write and/or execute permissions in the current directory: $(pwd)
 Please visit https://doc.oplist.org/guide/installation/docker#for-version-after-v4-1-0 for more information.
-错误：当前用户在当前目录没有写权限。
+错误：当前用户在当前目录没有写和/或执行权限。
 请访问 https://doc.oplist.org/guide/installation/docker#v4-1-0-%E4%BB%A5%E5%90%8E%E7%89%88%E6%9C%AC 获取更多信息。
 Exiting...
 EOF
     exit 1
   fi
 
+  # Define the target directory path for aria2 service
   ARIA2_DIR="/opt/service/start/aria2"
   if [ "$RUN_ARIA2" = "true" ]; then
     # If aria2 should run and target directory doesn't exist, copy it
